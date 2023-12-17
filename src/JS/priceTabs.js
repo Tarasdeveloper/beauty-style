d457262923dd48318834e38898b8e8bb348c273e
Tarasdeveloper <tomahavki1@gmail.com>
2023-12-17 19:49:51 +0200

    JS bug fix

--- src/JS/priceTabs.js
+++ src/JS/priceTabs.js
@@ -2,31 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
 	const tabs = document.querySelectorAll('.tab');
 	const tabContents = document.querySelectorAll('.pricing-content_item');
 
-	tabs.forEach(tab => {
+	tabs.forEach((tab, index) => {
 		tab.addEventListener('click', function () {
 			// Hide all tab contents
 			tabContents.forEach(content => {
-				content.style.visibility = 'hidden';
-			});
-
-			// Show the selected tab content
-			const tabId = this.getAttribute('data-tab');
-			const selectedTabContent = document.getElementById(tabId);
-			selectedTabContent.style.visibility = 'visible';
-		});
-	});
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-	const tabs = document.querySelectorAll('.tab');
-	const tabContents = document.querySelectorAll('.pricing-content_item');
-
-	tabs.forEach((tab, index) => {
-		tab.addEventListener('click', function () {
-			// Скрыть все вкладки
-			tabContents.forEach(content => {
-				content.setAttribute('hidden', 'true');
-				content.setAttribute('aria-hidden', 'true');
+				content.style.display = 'none';
 			});
 
 			// Убрать активный класс со всех вкладок
@@ -34,23 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
 				t.classList.remove('active');
 			});
 
-			// Показать выбранную вкладку
+			// Show the selected tab content
 			const tabId = this.getAttribute('data-tab');
 			const selectedTabContent = document.getElementById(tabId);
-			selectedTabContent.removeAttribute('hidden');
-			selectedTabContent.removeAttribute('aria-hidden');
+			selectedTabContent.style.display = 'block';
 
 			// Добавить активный класс к выбранной вкладке
 			this.classList.add('active');
 		});
 
-		// Установить первую вкладку активной по умолчанию
 		if (index === 0) {
 			tab.classList.add('active');
-			const tabId = tab.getAttribute('data-tab');
-			const selectedTabContent = document.getElementById(tabId);
-			selectedTabContent.removeAttribute('hidden');
-			selectedTabContent.removeAttribute('aria-hidden');
 		}
 	});
 });
