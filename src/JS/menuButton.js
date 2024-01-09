b539eeb84033b7f973b3d3e300f357d9f6015ed4
Tarasdeveloper <tomahavki1@gmail.com>
2024-01-09 22:46:17 +0200

    added socials mobile button

--- src/JS/menuButton.js
+++ src/JS/menuButton.js
@@ -1,15 +1,29 @@
 let navMenuBtn = document.querySelector('.nav-menu__btn');
 let navLinks = document.querySelector('.header-nav-links');
 let backDrop = document.querySelector('.header-nav-backdrop');
+let socButton = document.querySelector('.social-button');
+let socLinks = document.querySelector('.social-links');
 
 navMenuBtn.addEventListener('click', function () {
 	navMenuBtn.classList.toggle('active');
 	navLinks.classList.toggle('active');
 	backDrop.classList.toggle('active');
+	socLinks.classList.remove('active');
+	socButton.classList.remove('active');
 });
 
 backDrop.addEventListener('click', function () {
 	navMenuBtn.classList.remove('active');
 	navLinks.classList.remove('active');
+	backDrop.classList.remove('active');
+	socLinks.classList.remove('active');
+	socButton.classList.remove('active');
+});
+
+socButton.addEventListener('click', function () {
+	navMenuBtn.classList.remove('active');
+	navLinks.classList.remove('active');
 	backDrop.classList.toggle('active');
+	socLinks.classList.toggle('active');
+	socButton.classList.toggle('active');
 });
