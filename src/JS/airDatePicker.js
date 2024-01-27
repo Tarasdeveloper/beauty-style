1258d2fa4fefd845836485f963b2194074c29339
Tarasdeveloper <tomahavki1@gmail.com>
2024-01-27 12:40:51 +0200

    passed dates disabled

--- src/JS/airDatePicker.js
+++ src/JS/airDatePicker.js
@@ -1,9 +1,19 @@
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
+import localePl from 'air-datepicker/locale/pl';
+import localeEn from 'air-datepicker/locale/en';
+import localeDe from 'air-datepicker/locale/de';
+import localeUK from 'air-datepicker/locale/uk';
 
 new AirDatepicker('#appointmentPicker', {
 	// inline: true,
 	// isMobile: true,
-	// autoClose: true,
 	// position: 'left center',
+	// locale: [localePl, localeEn, localeUK, localeDe],
+	autoClose: true,
+	minDate: new Date(),
+	locale: localeDe,
+	locale: localeUK,
+	locale: localePl,
+	locale: localeEn,
 });
